8b5658c05e20692c9071a9c38e960276725e3c74
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-30 00:32:43 +0530

    initial commit

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.use("/api/items", itemRouter);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("client/dist"));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
     });
 }
 
